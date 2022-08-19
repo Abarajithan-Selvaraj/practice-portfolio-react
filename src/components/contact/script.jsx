f8af9629e8492a4b134bd61add550c080798d3ea
Abarajithan Selvaraj <abarajithan.s@icloud.com>
2022-08-20 00:20:07 +0530

    v0.1.0 practice done and deployed

--- src/components/contact/script.jsx
+++ src/components/contact/script.jsx
@@ -1,9 +1,61 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import './style.css';
+import {MdOutlineEmail} from 'react-icons/md'
+import {RiMessengerLine} from 'react-icons/ri'
+import { BsWhatsapp } from 'react-icons/bs'
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
+  const form = useRef()
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_vr7ioln', 'template_w3e4518', form.current, 'lpQxXuPM7r4Pc9ZBu')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+    
+    e.target.reset();
+  };
+
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get in touch</h5>
+      <h2>Contact me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>abdg027@gmail.com</h5>
+            <a href="mailto:abdg027@gmail.com" target="_blank" rel="noreferrer">Send a message</a>
+          </article>
+          <article className="contact__option">
+            <RiMessengerLine className='contact__option-icon'/>
+            <h4>Facebook</h4>
+            <h5>AbDG027</h5>
+            <a href="https://facebook.com" target="_blank" rel="noreferrer">Send a message</a>
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp className='contact__option-icon'/>
+            <h4>Whatsapp</h4>
+            <h5>+1 543 353 345344</h5>
+            <a href="https://api.whatsapp.com/send?phone+1543353345344" target="_blank" rel="noreferrer">Send a message</a>
+          </article>
+        </div>
+        {/* END OF CONTACT ACTION */}
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name="name" placeholder="Your fullname" required/>
+          <input type="email" name="email" placeholder="Your email" required />
+          <textarea name="message" rows="7" placeholder="Your message" required></textarea>
+          <button className="btn btn-primary" type="submit">Send message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
--- src/components/testimonials/script.jsx
+++ src/components/testimonials/script.jsx
@@ -1,10 +1,75 @@
 import React from 'react';
 import './style.css';
+import AVTR1 from '../../assets/avatar1.jpg'
+import AVTR2 from '../../assets/avatar2.jpg'
+import AVTR3 from '../../assets/avatar3.jpg'
+import AVTR4 from '../../assets/avatar4.jpg'
+
+// import Swiper core and required modules
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+const data = [
+  {
+    id: 1,
+    image: AVTR1,
+    name: "Client Name",
+    msg: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi pariatur quo, harum saepe hic reiciendis alias veritatis aliquam architecto ab ut ad, assumenda molestias adipisci excepturi, exercitationem repellendus nemo nihil."
+  },
+  {
+    id: 2,
+    image: AVTR2,
+    name: "Client Name",
+    msg: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi pariatur quo, harum saepe hic reiciendis alias veritatis aliquam architecto ab ut ad, assumenda molestias adipisci excepturi, exercitationem repellendus nemo nihil."
+  },
+  {
+    id: 3,
+    image: AVTR3,
+    name: "Client Name",
+    msg: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi pariatur quo, harum saepe hic reiciendis alias veritatis aliquam architecto ab ut ad, assumenda molestias adipisci excepturi, exercitationem repellendus nemo nihil."
+  },
+  {
+    id: 4,
+    image: AVTR4,
+    name: "Client Name",
+    msg: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi pariatur quo, harum saepe hic reiciendis alias veritatis aliquam architecto ab ut ad, assumenda molestias adipisci excepturi, exercitationem repellendus nemo nihil."
+  },
+]
 
 const Testimonials = () => {
   return (
-    <section id='testimonials'>Testimonials</section>
-  )
+    <section id='testimonials'>
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+
+      <Swiper
+        className="container testimonials__container"
+         // install Swiper modules
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{ clickable: true }}>
+        {
+          data.map(({ id, image, name, msg }) => {
+            return (
+              <SwiperSlide key={id} className="testimonial">
+                <div className="client__avatar">
+                  <img src={image} alt={name} />
+                </div>
+                <h5 className='client__name'>{name}</h5>
+                <small className="client__review">{msg}</small>
+              </SwiperSlide>
+            )
+          })
+        }
+      </Swiper>
+    </section>
+  ) 
 }
 
 export default Testimonials
